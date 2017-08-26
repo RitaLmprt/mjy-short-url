4d0065796e33bcf882d693e92fe45c1a0cfb24dc
System <none>
2017-08-26 09:14:50 +0000

    🍂🎥 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,62 @@
+// server.js
+// where node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient();
+var ObjectId = mongodb.ObjectId;
+var mongoURL = 'mongodb://maxi7587:urlshortenerdb@ds149433.mlab.com:49433/urlshortenerdb';
+var http = require('http');
+var url = require('url');
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/:data", function (request, response) {
+  var data = request.params.data;
+  if (data.length == 24){
+    var id = ObjectId(data);
+    var db = mongoClient.connect(mongoURL, function (err, db) {
+      var collection = db.collection('urls');
+      collection.findOne({_id: id}, {_id: 0, short_url: 0},function (err, document) {
+        var URL = document.original_url;
+        response.writeHead(301,{Location: URL});
+        response.end();
+        db.close();
+      });
+    });
+  } else {
+    response.send('Error: Wrong URL Are tou sure it´s OK?');
+  }
+});
+
+app.get("/new/http(s)?://:data", function (request, response) {
+  var data = url.parse(request.url).pathname.slice(5);
+  mongoClient.connect(mongoURL, function(err, db) {
+    var collection = db.collection('urls');
+    var id = ObjectId();
+    var insertData = {_id: id, original_url: data, short_url: 'https://my-short-url.glitch.me/' + id};
+    collection.insert(insertData, function () {
+      collection.findOne({_id: id}, {_id: 0}, function (err, document) {
+        response.send(document);
+        db.close();
+      });
+    });
+  });
+});
+
+app.get("/new/:data", function (request, response) {
+  response.send('Error: INVALID URL. Plase insert a valid URL. Use the right format and don´t forget to start with "http://" or "https://"')
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('App is listening on port ' + listener.address().port);
+});
